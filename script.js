fd15e2e52928b999a75f1431fb94483fa00a3bee
Oleg <olegsmarandi@gmail.com>
2022-03-03 17:21:22 +0300

    pushing lab1

---
+++ script.js
@@ -0,0 +1,20 @@
+login = document.getElementById("login");
+password = document.getElementById("password");
+loginBtn = document.getElementById("login_btn");
+form = document.getElementById("form");
+password.addEventListener("typing", (e) => {
+
+})
+
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log(login.value);
+    console.log(password.value);
+    const rawResponeNEW = fetch('v1/authorization', {
+        method: 'POST',
+        body: JSON.stringify({"password": password.value, "login": login.value})
+    });
+    const content = rawRespone.json();
+    console.log(content);
+    
+});
